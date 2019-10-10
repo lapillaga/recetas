efbd3a4c175817d89d64853b2dd5a0aa74c229ba
Luis Pillaga <lapillaga@espe.edu.ec>
2019-10-10 00:58:24 -0500

    Images for recipe

--- app/recipe/views.py
+++ app/recipe/views.py
@@ -1,4 +1,4 @@
-from rest_framework.decorators import action # custom action for viewset
+from rest_framework.decorators import action  # custom action for viewset
 from rest_framework.response import Response
 from rest_framework import viewsets, mixins, status
 from rest_framework.authentication import TokenAuthentication
